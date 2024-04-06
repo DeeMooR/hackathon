c086160bde9ba817470867d1eb75c1dcee40f39e
Dmitry Matyshev <deemoor4@gmail.com>
2024-04-06 21:07:36 +0300

    add BurgerMenu; add adaptive for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css'
 
 
 import logo from "src/img/icons/Logo.svg"
 import map from "src/img/icons/Map.svg"
+import menu from "src/img/icons/Location.svg"
+import { Link } from 'react-router-dom'
+import SlideBar from '../SlideBar'
 
 const Header = () => {
+  const [clickMenu, setClickMenu] = useState(false);
+
+  useEffect(() => {
+    if (clickMenu) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = 'auto';
+  }, [clickMenu]);
+
   return (
     <header className='header'>
       <div className="wrapper">
         <img src={logo} className='header__logo' alt="logo" />
         <nav className='header__nav'>
-          <a href="#">Ближайшие мероприятия</a>
-          <a href="#">Прошедшие мероприятия</a>
-          <a href="#">Контакты</a>
+          <Link to='/'>Ближайшие мероприятия</Link>
+          <Link to='/'>Прошедшие мероприятия</Link>
+          <Link to='/'>Контакты</Link>
+          <Link to='/' className='link-map'>Карта</Link>
         </nav>
         <div className="header__map">
           <img className='header__map-icon' src={map} alt="map" />
           <p className='header__map-text'>Карта корпусов</p>
         </div>
+        <img src={menu} alt="menu" className='header__menu' onClick={() => setClickMenu(true)} />
       </div>
+      <SlideBar clickMenu={clickMenu} setClickMenu={setClickMenu} />
     </header>
   )
 }
