37ac2c0b7787dd2bb41aadce45a1c4849b9fe98a
Dmitry Matyshev <deemoor4@gmail.com>
2024-04-07 15:34:08 +0300

    create EventPage

--- src/interface.ts
+++ src/interface.ts
@@ -3,3 +3,18 @@ export interface IContact {
   inst: string,
   telegram: string
 }
+
+export interface IEvent {
+  id: number,
+  photo: string,
+  title: string,
+  date: Date,
+  time: string,
+  location: string,
+  faculties: string[],
+  description: string,
+  archive: string,
+  results: string,
+  type: string,
+  visit: string
+}
